65ad4ba6f5a79654f7f3423bd269759b5aa5796f
INGALEXANDER <alexander_3_4@msn.com>
2021-10-07 20:27:44 -0500

    Alexander

--- LigaFutbolSoftInn.App.Persistencia/AppRepositorios/RepositorioEquipo.cs
+++ LigaFutbolSoftInn.App.Persistencia/AppRepositorios/RepositorioEquipo.cs
@@ -41,6 +41,7 @@ namespace LigaFutbolSoftInn.App.Persistencia
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         }
 
@@ -78,6 +79,8 @@ namespace LigaFutbolSoftInn.App.Persistencia
             }
             return null;
 >>>>>>> alexander2
+=======
+>>>>>>> 9d5046e88f27a3e023fec3a467a1a2fcc36cbdf7
 =======
 >>>>>>> 9d5046e88f27a3e023fec3a467a1a2fcc36cbdf7
 =======
